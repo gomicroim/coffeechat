b7b04001347324279ea781c2ee750f3aa46f7f98
xmcy0011 <xmcy0011@sina.com>
2022-09-16 18:19:55 +0800

    refactor java chat api gen shell

--- v2/deploy/shell/gen_java_pb.sh
+++ v2/deploy/shell/gen_java_pb.sh
@@ -1,25 +1,32 @@
 #!/bin/sh
 
 CUR_DIR=`cd ../../ && pwd`
-API_USER_DIR=${CUR_DIR}/api/apiuser/api/user/v1
+API_USER_DIR=${CUR_DIR}/api/apiuser/api/user/v1/
+API_CHAT_DIR=${CUR_DIR}/api/apichat/api/chat/v1/
+API_CHAT_CONSTANT_DIR=${CUR_DIR}/rpc/chat/api/chat/constant.proto
 THIRD_PARTY_DIR=${CUR_DIR}/third_party
 
 PROTOC_DIRS="
 ${API_USER_DIR}
+${API_CHAT_DIR}
+${API_CHAT_CONSTANT_DIR}
 "
 
 echo "generate start..."
 for dir in ${PROTOC_DIRS}; do
-    for file in ${dir}/*.proto; do
-        # go
-        # protoc --proto_path=${dir} --proto_path=${CUR_DIR} --proto_path=${THIRD_PARTY_DIR} --go-grpc_out=${CUR_DIR} --go_out=${CUR_DIR} ${file}
-
-        if [[ ! ${file} =~ "api_" ]]; then
-          # java
-          protoc --proto_path=${dir} --proto_path=${CUR_DIR} --proto_path=${THIRD_PARTY_DIR} --java_out=${CUR_DIR} ${file}
-          echo "gen ${file} ok"
-        fi
-
-    done
+    if [[ ${dir} == */ ]];then
+      for file in ${dir}*.proto; do
+          # go
+          # protoc --proto_path=${dir} --proto_path=${CUR_DIR} --proto_path=${THIRD_PARTY_DIR} --go-grpc_out=${CUR_DIR} --go_out=${CUR_DIR} ${file}
+          if [[ ! ${file} =~ "api_" ]]; then
+            # java
+            protoc --proto_path=${dir} --proto_path=${CUR_DIR} --proto_path=${THIRD_PARTY_DIR} --java_out=${CUR_DIR} ${file}
+            echo "gen ${file} ok"
+          fi
+      done
+    else
+      protoc --proto_path=${CUR_DIR} --proto_path=${THIRD_PARTY_DIR} --java_out=${CUR_DIR} ${dir}
+      echo "gen ${dir} ok"
+    fi
 done
 echo "generate end!"
